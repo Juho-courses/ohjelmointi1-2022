68ae21bfdcc496930c3ff82f73bcbec8386531a9
Juho S <juho.salli@gmail.com>
2022-12-05 11:39:18 +0200

    funktioharkka 3

---
+++ funktiot/harjoitus-03/Program.cs
@@ -0,0 +1,10 @@
+﻿void TulostaTaulukko(int[] taulukko)
+{
+    foreach (int luku in taulukko)
+    {
+        Console.WriteLine(luku);
+    }
+}
+
+int[] taulukko = { 1, 2, 3, 4, 5 };
+TulostaTaulukko(taulukko);
